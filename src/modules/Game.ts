e4dc5cbb90a507e0c650d510d6e481e5f755529f
Ping Zhang <pz771114@gmail.com>
2022-09-20 13:58:16 -0400

    Game class

--- src/modules/Game.ts
+++ src/modules/Game.ts
@@ -22,4 +22,15 @@ class Game {
     this.direction = "";
     this.isGameOver = false;
   }
+
+  control(){
+
+    //How can we monitor if user pressed arrow keys 
+
+    //How can we tell which arrow key use pressed, up,down,left or right?
+
+    //What should we do after user pressed arrow keys
+
+    
+  }
 }
